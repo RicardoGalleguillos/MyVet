c87a278b3ca422cf5377f045e707e61f3a605447
Ricardo Galleguillos <rgalleguillos26@gmail.com>
2020-01-03 05:38:00 -0300

    Add user entities

--- MyVet.Web/Startup.cs
+++ MyVet.Web/Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyVet.Web.Data;
+using MyVet.Web.Data.Entities;
+using MyVet.Web.Helpers;
 
 namespace MyVet.Web
 {
@@ -33,13 +36,37 @@ namespace MyVet.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            //User es la clase de usuarios personalizada, como no hay clase de roles personalizadas se usa IdentityRole
+            //y se configuran las reglas de autenticación de usuario y password
+            services.AddIdentity<User, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+                cfg.Password.RequireDigit = false;
+                cfg.Password.RequiredUniqueChars = 0;
+                cfg.Password.RequireLowercase = false;
+                cfg.Password.RequireNonAlphanumeric = false;
+                cfg.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<DataContext>();
+
+
             services.AddDbContext<DataContext>(cfg =>
             {
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
 
+
+            //Se recomienda Inyectar con interfaz, ya que permite cambiar una implementación en tiempo de ejecución
+            //y es mas facil el testeo de la aplicación (Uno de los principios solid es que la aplicación sea testeable)
+
+            //Injecta una vez
             services.AddTransient<SeedDb>();
+            //cualquier clase que utilice IUserHelper en el contructor, se le envía un objeto UserHlper (DI)
+            //Crea una instancia de UserHelper cada vez que lo Inyectemos en un constructor, Ejemplo: Si en un constructor solicitamos UserHelper, se crea una nueva instacia
+            //de UserHelper, tantas instancias como sean solicitadas.
+            services.AddScoped<IUserHelper, UserHelper>();
+            //Inyecta una vez, pero deja el objeto creado siempre en memoria y no es necesario inyectarlo cada vez. 
+            //services.AddSingleton<UserHelper>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -58,6 +85,8 @@ namespace MyVet.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            //Al crear usuario con roles (login) se debe agregar  app.UseAuthentication();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
